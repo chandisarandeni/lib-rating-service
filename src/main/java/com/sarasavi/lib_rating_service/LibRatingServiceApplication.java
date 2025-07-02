e4ea5e7b0687c04d3490a8c0cce470daf74b0c95
chandisarandeni <chandisarandeni2003@gmail.com>
2025-07-02 22:09:05 +0530

    feat: add ModelMapper and RestTemplate beans ✅

--- src/main/java/com/sarasavi/lib_rating_service/LibRatingServiceApplication.java
+++ src/main/java/com/sarasavi/lib_rating_service/LibRatingServiceApplication.java
@@ -6,14 +6,27 @@
 
 package com.sarasavi.lib_rating_service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LibRatingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibRatingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibRatingServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
