c9a8de58bf9c5969277ea92c0fa9c55ca35bd4f1
chandisarandeni <chandisarandeni2003@gmail.com>
2025-07-02 22:03:42 +0530

    init: project with spring, jpa and jdbc 🚀

---
+++ src/main/java/com/sarasavi/lib_rating_service/LibRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_rating_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibRatingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarasavi/lib_rating_service/LibRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarasavi.lib_rating_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
