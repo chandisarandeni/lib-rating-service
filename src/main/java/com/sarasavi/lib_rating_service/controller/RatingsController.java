2d74bb6f45afc382d90709c6322a6a57e55f20b0
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 13:18:56 +0530

    feat: Add base annotations and dependencies to RatingsController ✅

--- src/main/java/com/sarasavi/lib_rating_service/controller/RatingsController.java
+++ src/main/java/com/sarasavi/lib_rating_service/controller/RatingsController.java
@@ -1,4 +1,24 @@
 package com.sarasavi.lib_rating_service.controller;
 
+import com.sarasavi.lib_rating_service.service.RatingsService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+
 public class RatingsController {
+
+    @Autowired
+    private RatingsService ratingsService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // Get all ratings
+    
 }
