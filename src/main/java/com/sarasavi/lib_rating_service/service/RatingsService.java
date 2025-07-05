9774caad507cd74760c6071702baf2df50c293b9
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 12:34:02 +0530

    feet: implimet the get all ratings service  ✅

--- src/main/java/com/sarasavi/lib_rating_service/service/RatingsService.java
+++ src/main/java/com/sarasavi/lib_rating_service/service/RatingsService.java
@@ -1,10 +1,14 @@
 package com.sarasavi.lib_rating_service.service;
 
+import com.sarasavi.lib_rating_service.dto.RatingsDTO;
+import com.sarasavi.lib_rating_service.entity.Ratings;
 import com.sarasavi.lib_rating_service.repository.RatingsRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class RatingsService {
@@ -16,4 +20,12 @@ public class RatingsService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    //get all ratings
+    public List<RatingsDTO> getAllRatings() {
+        List<Ratings> ratingsList = ratingsRepository.findAll();
+        return ratingsList.stream()
+                .map(ratings -> modelMapper.map(ratings, RatingsDTO.class))
+                .toList();
+    }
 }
