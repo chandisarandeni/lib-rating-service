a5ba6ed007e6ee0cad24aa2737bf942f113b904f
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 13:29:32 +0530

    feat: Implement the get all ratings in controller class ✅

--- src/main/java/com/sarasavi/lib_rating_service/controller/RatingsController.java
+++ src/main/java/com/sarasavi/lib_rating_service/controller/RatingsController.java
@@ -1,11 +1,15 @@
 package com.sarasavi.lib_rating_service.controller;
 
+import com.sarasavi.lib_rating_service.dto.RatingsDTO;
 import com.sarasavi.lib_rating_service.service.RatingsService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -20,5 +24,10 @@ public class RatingsController {
     private ModelMapper modelMapper;
 
     // Get all ratings
-    
+    @GetMapping(path = "/ratings")
+    public List<RatingsDTO> getAllRatings() {
+        return ratingsService.getAllRatings();
+    }
+
+
 }
