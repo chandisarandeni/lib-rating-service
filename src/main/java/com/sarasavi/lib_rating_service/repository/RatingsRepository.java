e4acc8aa387457ebbc1042314a4472cb3d4c35ee
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 12:40:47 +0530

    chore: wrote an advance quarry for find ratings by member id ✅

--- src/main/java/com/sarasavi/lib_rating_service/repository/RatingsRepository.java
+++ src/main/java/com/sarasavi/lib_rating_service/repository/RatingsRepository.java
@@ -2,7 +2,14 @@ package com.sarasavi.lib_rating_service.repository;
 
 import com.sarasavi.lib_rating_service.entity.Ratings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RatingsRepository extends JpaRepository<Ratings, Integer> {
+
+    //find ratings by member id
+    @Query("SELECT r FROM Ratings r WHERE r.memberId = ?1")
+    List<Ratings> findByMemberId(int memberId);
 
 }
