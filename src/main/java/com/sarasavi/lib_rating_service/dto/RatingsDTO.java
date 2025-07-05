b6f5c32e572ff3da4c7c445f7a744a5c7c862ef5
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 11:09:31 +0530

    Feet: implement the ratings DTO class ✅

--- src/main/java/com/sarasavi/lib_rating_service/dto/RatingsDTO.java
+++ src/main/java/com/sarasavi/lib_rating_service/dto/RatingsDTO.java
@@ -1,4 +1,17 @@
 package com.sarasavi.lib_rating_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RatingsDTO {
+    private int ratingId;
+    private int bookId;
+    private int memberId;
+    private int stars;
+    private String review;
+    private String dateOfRating;
 }
