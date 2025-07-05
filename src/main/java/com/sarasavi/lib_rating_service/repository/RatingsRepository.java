7b59a03f9be79a5796cb525eb326b4ced9349ad1
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 11:26:28 +0530

    chore: updated the RatingsRepository

--- src/main/java/com/sarasavi/lib_rating_service/repository/RatingsRepository.java
+++ src/main/java/com/sarasavi/lib_rating_service/repository/RatingsRepository.java
@@ -1,4 +1,8 @@
 package com.sarasavi.lib_rating_service.repository;
 
-public interface RatingsRepository {
+import com.sarasavi.lib_rating_service.entity.Ratings;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RatingsRepository extends JpaRepository<Ratings, Integer> {
+
 }
