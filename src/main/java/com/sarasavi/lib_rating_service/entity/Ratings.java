2df7e6d90d08c139612f01362d881fd7d396b955
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 10:59:16 +0530

    Implement the Ratings Entity Class ✅

--- src/main/java/com/sarasavi/lib_rating_service/entity/Ratings.java
+++ src/main/java/com/sarasavi/lib_rating_service/entity/Ratings.java
@@ -1,4 +1,34 @@
 package com.sarasavi.lib_rating_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "ratings")
+
 public class Ratings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rating_id")
+    private int ratingId;
+
+    @Column(name = "book_id")
+    private int bookId;
+
+    @Column(name = "member_id")
+    private int memberId;
+
+    @Column(name = "stars")
+    private int stars;
+
+    @Column(name = "review")
+    private String review;
+
+    @Column(name = "date_of_rating")
+    private String dateOfRating;
 }
