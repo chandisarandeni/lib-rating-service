2ec550c1b52167b579505fdd476b19ce91c55599
Saniru Dewmina <sanirudewmina9@gmail.com>
2025-07-05 11:27:17 +0530

    chore: Add dependencies and annotations to RatingsService ✅

--- src/main/java/com/sarasavi/lib_rating_service/service/RatingsService.java
+++ src/main/java/com/sarasavi/lib_rating_service/service/RatingsService.java
@@ -1,4 +1,19 @@
 package com.sarasavi.lib_rating_service.service;
 
+import com.sarasavi.lib_rating_service.repository.RatingsRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class RatingsService {
+    @Autowired
+    private RatingsRepository ratingsRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private RestTemplate restTemplate;
 }
